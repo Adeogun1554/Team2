162fe87e66e819b67c18279e21e7bd66f3ecbf37
Adeogun1554 <emmadeogun@gmail.com>
2025-07-02 19:17:45 +0000

    Import os for PORT binding

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -15,4 +16,5 @@ def review_handler():
     return jsonify(reviews)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
+    app.run(host='0.0.0.0', port=port)
